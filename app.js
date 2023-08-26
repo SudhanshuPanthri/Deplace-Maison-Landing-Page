038817aff12cde79c168ef5a28ef31102dcffe09
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2023-08-26 19:32:08 +0530

    commit

---
+++ app.js
@@ -0,0 +1,117 @@
+function init() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
+const circleFollowsMouse = () => {
+  window.addEventListener("mousemove", (details) => {
+    document.querySelector(
+      "#cursor"
+    ).style.transform = `translate(${details.clientX}px,${details.clientY}px)`;
+  });
+};
+
+init();
+
+circleFollowsMouse();
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#page1 h1",
+    scroller: "#main",
+    // markers: true,
+    start: "top 13%",
+    end: "top 50%",
+    scrub: 3,
+  },
+});
+
+let tl2 = gsap.timeline();
+
+tl2
+  .from("#page1 h1", {
+    y: 200,
+    opacity: 0,
+  })
+  .to("#page1 h1", {
+    y: -100,
+    opacity: 1,
+  })
+  .from("#page1 h2", {
+    y: 100,
+    opacity: 0,
+  })
+  .to("#page1 h2", {
+    y: 0,
+    opacity: 1,
+  })
+  .from("#page1 #img-container>img", {
+    y: 200,
+    opacity: 0,
+  })
+  .to("#page1 #img-container>img", {
+    y: 0,
+    opacity: 1,
+  });
+
+tl.to("#page1 #img-container>img", {
+  scale: 1.05,
+});
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#page2 #p2-content h1",
+    scroller: "#main",
+    markers: true,
+    start: "top 103%",
+    end: "top 50%",
+    scrub: 3,
+  },
+});
+
+tl3
+  .from("#page2 #p2-content h1", {
+    opacity: 0,
+    y: 200,
+  })
+  .to("#page2 #p2-content h1", {
+    opacity: 1,
+    y: 0,
+  })
+  .from("#page2 #drag-div", {
+    opacity: 0,
+    x: 200,
+  })
+  .to("#page2 #drag-div", {
+    opacity: 1,
+    x: 0,
+  });
